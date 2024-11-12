7ccd3b984014ab165f155c913a87a2f01c78a28c
jonathan <jtmacoco@mail.csuchico.edu>
2024-11-11 22:27:14 -0800

    code editor that uses sockets to brodcast changes

---
+++ distributed_code_editor/src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+import Document from './components/Document/Document';
+function App() {
+  return (
+    Document()
+  );
+}
+
+export default App;
---
+++ distributed_code_editor/src/components/Document/Document.js
@@ -0,0 +1,55 @@
+import { Editor } from "@monaco-editor/react";
+import './Document.css'
+import { useEffect, useState, useRef, useCallback } from 'react'
+import io from 'socket.io-client';
+function Document() {
+    const [status, setStatus] = useState('Disconnected');
+    const editorRef = useRef(null);
+    const [socket, setSocket] = useState()
+    const isRemoteUpdate = useRef(false); 
+    const editorDidMount = (editor, monaco) => {
+        console.log("Editor is mounted", editor);
+        editorRef.current = editor;
+    };
+
+    useEffect(() => {
+        const s = io('http://localhost:8000')
+        setSocket(s)
+        setStatus('Connect')
+        s.on('documentUpdate', (content) => {
+            if (editorRef.current) {
+                isRemoteUpdate.current=true
+                editorRef.current.setValue(content);
+            }
+        });
+        return () => {
+            setStatus('Disconnect')
+            s.disconnect()
+        }
+    }, [])
+  const handleEditorChange = useCallback((value, event) => {
+        if(isRemoteUpdate.current){
+            isRemoteUpdate.current=false
+            return
+        }
+        if (value.trim()&&socket) {
+            console.log(value.trim())
+            console.log("Emitting document update to server");
+            socket.emit('documentUpdate', value.trim())
+        }
+    },[socket])
+  
+    return (
+        <div>
+            <div id="status">{status}</div>
+            <Editor
+                height="100vh"
+                width="100%"
+                theme="vs-dark"
+                onChange={handleEditorChange}
+                onMount={editorDidMount}
+            />
+        </div>
+    )
+}
+export default Document
