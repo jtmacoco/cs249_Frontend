c21984948699d9841c4c707717aef0da6dc5d2db
jonathan <jtmacoco@mail.csuchico.edu>
2024-11-15 21:15:23 -0800

    added simple routing

--- distributed_code_editor/src/App.js
+++ distributed_code_editor/src/App.js
@@ -1,8 +1,16 @@
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Document from './components/Document/Document';
+import { v4 as uuidV4 } from 'uuid'
 function App() {
   return (
-    Document()
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to={`/documents/${uuidV4()}`} />} />
+        <Route path="/documents/:id" element={<Document />} />
+      </Routes>
+    </Router>
   );
 }
 
