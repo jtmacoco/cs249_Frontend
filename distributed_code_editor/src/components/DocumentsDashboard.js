6e87bb5f32e33592e515784e282d4f7c80b31036
Eric Wu <ericwuew2266@Erics-MacBook-Air.local>
2024-11-30 17:32:48 -0800

    removed incorrect import of css

--- distributed_code_editor/src/components/DocumentsDashboard.js
+++ distributed_code_editor/src/components/DocumentsDashboard.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import "./DocumentsDashboard.css";
 
 const DocumentsDashboard = ({ username }) => {
     const [documents, setDocuments] = useState([]); // List of shared documents
