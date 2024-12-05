7e2a729a99c9c3762e972db63cc0ea235ef76716
EW2266 <ericwu2266@gmail.com>
2024-12-04 23:50:34 -0800

    changed correct baseUrl

--- distributed_code_editor/src/components/constants/Endpoints.js
+++ distributed_code_editor/src/components/constants/Endpoints.js
@@ -1,5 +1,5 @@
 class EndPoint {
-    static baseUrl = "http://3.130.25.213:8000/api/";
+    static baseUrl = "http://3.130.25.213/api/";
     //static baseUrl = "http://localhost:8000/api/";
     static register = 'user/register';
     static login = 'user/login';
